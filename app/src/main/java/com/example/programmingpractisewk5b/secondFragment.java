8c37f10c74bfc7d9f3a81ff34f57e26ccc05597b
JTRYI <108774025+JTRYI@users.noreply.github.com>
2022-07-20 10:27:42 +0800

    First commit

--- app/src/main/java/com/example/programmingpractisewk5b/secondFragment.java
+++ app/src/main/java/com/example/programmingpractisewk5b/secondFragment.java
@@ -1,5 +1,7 @@
 package com.example.programmingpractisewk5b;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class secondFragment extends Fragment {
+
+    Activity context;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -58,7 +63,21 @@ public class secondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        context = getActivity();
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+    public void onStart(){
+        super.onStart();
+        TextView textView1 = (TextView)  context.findViewById(R.id.MyFavourites);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (context,MyFavouritesPlaylist.class);
+                startActivity(intent);
+            }
+        });
     }
 }
